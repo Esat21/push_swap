d17c0adf3faf9cfca632d7a0799107e9ae5ef01a
Serhii Sokhatskyi <ssokhats@c2r3s4.42warsaw.pl>
2026-07-13 21:13:42 +0200

    stack creation implemented

--- src/stack_creation.c
+++ src/stack_creation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   stack_creation.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: esyaman <esyaman@student.42warsaw.pl>      +#+  +:+       +#+        */
+/*   By: ssokhats <ssokhats@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 18:46:13 by esyaman           #+#    #+#             */
-/*   Updated: 2026/07/13 18:49:30 by esyaman          ###   ########.fr       */
+/*   Updated: 2026/07/13 21:12:13 by ssokhats         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 t_stack	*create_stack_a(int argc, char **argv)
 {
-	
+	int	i;
+	t_stack *stack_a;
+
+	i = 0;
+	stack_a = stacknew(argv[0]); //todo atoi(argv[0]) when libft will be added
+	while (++i < argc)
+		stack_add_back(&stack_a, stacknew(argv[0])); //same shit
+	return (stack_a);
 }
