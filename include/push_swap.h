c687601f24761569153449fdc93c937ccdbe3599
Serhii Sokhatskyi <ssokhats@c2r2s3.42warsaw.pl>
2026-07-16 13:22:48 +0200

    chenged stack struct logic

--- include/push_swap.h
+++ include/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: esyaman <esyaman@student.42warsaw.pl>      +#+  +:+       +#+        */
+/*   By: ssokhats <ssokhats@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 18:06:09 by esyaman           #+#    #+#             */
-/*   Updated: 2026/07/16 12:51:13 by esyaman          ###   ########.fr       */
+/*   Updated: 2026/07/16 13:11:40 by ssokhats         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,19 @@
 
 # include <stdlib.h>
 
-typedef struct s_stack
+typedef struct s_node
 {
 	int		value;
-	t_stack	*next;
+	t_node	*next;
+}			t_node;
+
+typedef struct s_stack
+{
+	t_node	*first;
+	t_node	*last;
+	size_t	size;
 }			t_stack;
+
 
 void	stack_add_back(t_stack **lst, t_stack *new);
 t_stack	*stacklast(t_stack *lst);
