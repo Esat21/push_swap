c6e3c893b413eade91e3e94db33e20ae37047313
Serhii Sokhatskyi <sokhatsky.98@gmail.com>
2026-08-02 13:20:32 +0200

    empty strings as an argument are handled.

--- src/flattern_args.c
+++ src/flattern_args.c
@@ -17,9 +17,12 @@ void	free_split(char **split)
 	int	i;
 
 	i = 0;
-	while (split[i])
-		free(split[i++]);
-	free(split);
+	if (split)
+	{
+		while (split[i])
+			free(split[i++]);
+		free(split);
+	}
 }
 
 static int count_tokens(int argc, char **argv)
